5d083808b6877c7345e8f52f12aa5a08d5087f72
Jonny Hork <jhork@salesforce.com>
2020-12-21 15:08:08 -0700

    show and hide options, render selected fields

--- src/modules/my/customSelect/customSelect.ts
+++ src/modules/my/customSelect/customSelect.ts
@@ -3,9 +3,20 @@ import { mockMDT } from '../model';
 
 export default class CustomSelect extends LightningElement {
   @api allFields: string[] = mockMDT.fields.Account;
-  @api selectedFields: string[];
+  @api selectedFields: string[] = [];
   @track _renderedFields: string[] = [];
   searchTerm: string = '';
+  optionsWrapper: HTMLElement;
+
+  connectedCallback() {
+    document.addEventListener('click', (e) => {
+      this.handleClose(e);
+    });
+  }
+
+  renderedCallback() {
+    this.optionsWrapper = this.template.querySelector('.options__wrapper');
+  }
 
   filterFields() {
     if (this.searchTerm) {
@@ -18,9 +29,33 @@ export default class CustomSelect extends LightningElement {
     }
   }
 
+  handleClose(e) {
+    e.preventDefault();
+    this.optionsWrapper.classList.remove('options--open');
+  }
+
+  handleOpen(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    const optionsWrapper = this.template.querySelector('.options__wrapper');
+    optionsWrapper.classList.add('options--open');
+    this.filterFields();
+  }
+
   handleFieldSearch(e) {
     e.preventDefault();
     this.searchTerm = e.target.value;
     this.filterFields();
+  }
+
+  handleFieldSelected(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    const optionValue = e.target.getAttribute('data-option-value');
+    if (optionValue) {
+      this.selectedFields = [...this.selectedFields, optionValue];
+    } else {
+      console.log('NO Option Value!');
+    }
   }
 }
